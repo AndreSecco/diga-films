98e44ed29edd4aabbab6393052838ec732ac05e2
AndreSecco <andrefigueiro5@gmail.com>
2022-10-20 15:27:18 -0300

    test development for login integration with backend and cookie storage

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/User/LoginComponent.vue'
+import Guard from '../services/middleware'
 
 const routes = [
   {
@@ -10,10 +12,16 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    beforeEnter: Guard.auth,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
