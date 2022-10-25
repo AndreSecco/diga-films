0be315f1f2c9ec1119e03c0af6f6ed5ea574f081
AndreSecco <andrefigueiro5@gmail.com>
2022-10-25 00:58:29 -0300

    Finished CRUD for tags and films

--- backend/app/Http/Controllers/TagController.php
+++ backend/app/Http/Controllers/TagController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    public function addTag(Request $request){
+        $newTag = new Tag();
+
+        $newTag->nameTag = $request->nameTag;
+        $newTag->save();
+        
+        return response()->json('Success');
+
+    }
+
+    public function getTags(){
+        $tags = Tag::all();
+
+        return response()->json($tags);
+    }
+    public function editTag(Tag $id){
+        return response()->json($id);
+    }
+
+    public function deleteTag(Tag $id, Request $request){
+        $id->delete();
+        
+        return response()->json('Success');
+    }
+
+    public function updateTag(Tag $id, Request $request){
+        $id->nameTag = $request->nameTag;
+
+        $id->save();
+
+        return response()->json('Success');
+    }
+
+    public function singleTag(Tag $id){
+        return response()->json($id);
+    }
 }
